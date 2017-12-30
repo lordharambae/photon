dc6ab65791b34c04f869a7f903b91b02b001791a
KSL <lordharambae@gmail.com>
2017-12-30 12:19:38 +0530

    modified

--- photon/edge.hpp
+++ photon/edge.hpp
@@ -2,19 +2,49 @@
 #ifndef EDGE_HPP
 #define EDGE_HPP
 
+
 namespace photon {
 
 	class edge {
 
-		
+		vertex _first;
+		vertex _second;
+		float _efficiency;
 		
 	public:
 
+		edge();
+		edge(const vertex& _first, const vertex& _second, const float& _efficiency);
+		~edge();
+
+		const constexpr vertex& first() const noexcept;
+		const constexpr vertex& second() const noexcept;
+		const constexpr float& efficiency() const noexcept;
 
 	};
 
-	};
+}
 
+photon::edge::edge() {}
+
+photon::edge::edge(const vertex& _first, const vertex& _second, const float& _efficiency) {
+	this->_first = _first;
+	this->_second = _second;
+	this->_efficiency = _efficiency;
+}
+
+photon::edge::~edge() {}
+
+const constexpr photon::vertex& photon::vertex::first() const noexcept {
+	return this->_first;
+}
+
+const constexpr photon::vertex& photon::vertex::second() const noexcept {
+	return this->_second;
+}
+
+const constexpr float& photon::vertex::efficiency() const noexcept {
+	return this->_efficiency;
 }
 
 #endif
--- photon/vertex.hpp
+++ photon/vertex.hpp
@@ -2,6 +2,8 @@
 #ifndef VERTEX_HPP
 #define VERTEX_HPP
 
+#include <iostream>
+
 namespace photon {
 
 	class vertex {
@@ -14,8 +16,6 @@ namespace photon {
 	public:
 
 		vertex();
-		vertex(const float& _x, const float& _y, const float& _z);
-		vertex(const float& _delay);
 		vertex(const float& _x, const float& _y, const float& _z, const float& _delay);
 		~vertex();
 
@@ -24,8 +24,41 @@ namespace photon {
 		const constexpr float& z() const noexcept;
 		const constexpr float& delay() const noexcept;
 
+		operator std::ostream() {
+			std::ostream os;
+			os << this->_x << this->_y << this->_z << this->_delay;
+		}
+
 	};
 
 }
+
+photon::vertex::vertex() {}
+
+photon::vertex::vertex(const float& _x, const float& _y, const float& _z, const float& _delay) {
+	this->_x = _x;
+	this->_y = _y;
+	this->_z = _z;
+	this->_delay = _delay;
+}
+
+photon::vertex::~vertex() {}
+
+const constexpr float& photon::vertex::x() const noexcept {
+	return this->_x;
+}
+
+const constexpr float& photon::vertex::y() const noexcept {
+	return this->_y;
+}
+
+const constexpr float& photon::vertex::z() const noexcept {
+	return this->_z;
+}
+
+const constexpr float& photon::vertex::delay() const noexcept {
+	return this->_delay;
+}
+
 
 #endif
